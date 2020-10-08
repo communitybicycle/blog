4b6b7e764af790e38d5aaf1da421f91ce6c30406
Hal Shin <hal.shin@alumni.ubc.ca>
2020-10-07 22:43:19 -0700

    Undo graphql changes

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,36 +1,26 @@
 // @flow strict
-import React from "react";
-import { graphql } from "gatsby";
-import Layout from "../components/Layout";
-import Post from "../components/Post";
-import { useSiteMetadata } from "../hooks";
-import type { MarkdownRemark } from "../types";
+import React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import Post from '../components/Post';
+import { useSiteMetadata } from '../hooks';
+import type { MarkdownRemark } from '../types';
 
 type Props = {
   data: {
-    markdownRemark: MarkdownRemark,
-  },
+    markdownRemark: MarkdownRemark
+  }
 };
 
 const PostTemplate = ({ data }: Props) => {
   const { title: siteTitle, subtitle: siteSubtitle } = useSiteMetadata();
   const { frontmatter } = data.markdownRemark;
-  const {
-    title: postTitle,
-    description: postDescription,
-    socialImage,
-  } = frontmatter;
-  const metaDescription =
-    postDescription !== null ? postDescription : siteSubtitle;
-  const socialImageUrl =
-    typeof socialImage !== "undefined" ? socialImage["publicURL"] : undefined;
+  const { title: postTitle, description: postDescription, socialImage } = frontmatter;
+  const metaDescription = postDescription !== null ? postDescription : siteSubtitle;
+  const socialImageUrl = typeof socialImage !== 'undefined' ? socialImage['publicURL'] : undefined;
 
   return (
-    <Layout
-      title={`${postTitle} - ${siteTitle}`}
-      description={metaDescription}
-      socialImage={socialImageUrl}
-    >
+    <Layout title={`${postTitle} - ${siteTitle}`} description={metaDescription} socialImage={socialImageUrl} >
       <Post post={data.markdownRemark} />
     </Layout>
   );
@@ -50,7 +40,9 @@ export const query = graphql`
         description
         tags
         title
-        socialImage
+        socialImage {
+          publicURL
+        }
       }
     }
   }
